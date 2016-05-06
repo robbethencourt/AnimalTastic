fa5ce987a13fe61e6c3327a88efdb5f4b6511752
Robert Bethencourt <robert.bethencourt@gmail.com>
2016-05-06 16:06:01 -0400

    added array of animals

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,238 @@
 // Javascript function that wraps everything
 $(document).ready(function(){
 
+	function animalTastic() {
+		
+		var animals = [
+			{
+				name: 'Amur Leopard',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Black Rhino',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Cross River Gorilla',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Hawksbill Turtle',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Javan Rhino',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Leatherback Turtle',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Mountain Gorilla',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Pangolin',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Saola',
+				status: 'Critically Endangered'
+			}, {
+				name: 'South China Tiger',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Sumatran Elephant',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Sumatran Orangutan',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Sumatran Rhino',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Sumatran Tiger',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Vaquita',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Western Lowland Gorilla',
+				status: 'Critically Endangered'
+			}, {
+				name: 'Yangtze Finless Porpoise',
+				status: 'Critically Endangered'
+			}, {
+				name: 'African Wild Dog',
+				status: 'Endangered'
+			}, {
+				name: 'Amur Tiger',
+				status: 'Endangered'
+			}, {
+				name: 'Asian Elephant',
+				status: 'Endangered'
+			}, {
+				name: 'Bengal Tiger',
+				status: 'Endangered'
+			}, {
+				name: 'Black Spider Monkey',
+				status: 'Endangered'
+			}, {
+				name: 'Black-footed Ferret',
+				status: 'Endangered'
+			}, {
+				name: 'Blue Whale',
+				status: 'Endangered'
+			}, {
+				name: 'Bluefin Tuna',
+				status: 'Endangered'
+			}, {
+				name: 'Bonobo',
+				status: 'Endangered'
+			}, {
+				name: 'Bornean Orangutan',
+				status: 'Endangered'
+			}, {
+				name: 'Borneo Pygmy Elephant',
+				status: 'Endangered'
+			}, {
+				name: 'Chimpanzee',
+				status: 'Endangered'
+			}, {
+				name: 'Eastern Lowland Gorilla',
+				status: 'Endangered'
+			}, {
+				name: 'Fin Whale',
+				status: 'Endangered'
+			}, {
+				name: 'Galapagos Penguin',
+				status: 'Endangered'
+			}, {
+				name: 'Ganges River Dolphin',
+				status: 'Endangered'
+			}, {
+				name: 'Giant Panda',
+				status: 'Endangered'
+			}, {
+				name: 'Green Turtle',
+				status: 'Endangered'
+			}, {
+				name: "Hector's Dolphin",
+				status: 'Endangered'
+			}, {
+				name: 'Humphead Wrasse',
+				status: 'Endangered'
+			}, {
+				name: 'Indian Elephant',
+				status: 'Endangered'
+			}, {
+				name: 'Indohinese Tiger',
+				status: 'Endangered'
+			}, {
+				name: 'Loggerhead Turtle',
+				status: 'Endangered'
+			}, {
+				name: 'Malayan Tiger',
+				status: 'Endangered'
+			}, {
+				name: 'North Atlantic Right Whale',
+				status: 'Endangered'
+			}, {
+				name: 'Orangutan',
+				status: 'Endangered'
+			}, {
+				name: 'Sea Lions',
+				status: 'Endangered'
+			}, {
+				name: 'Snow Leopard',
+				status: 'Endangered'
+			}, {
+				name: 'Sri Lankan Elephant',
+				status: 'Endangered'
+			}, {
+				name: 'Tiger',
+				status: 'Endangered'
+			}, {
+				name: 'Whale',
+				status: 'Endangered'
+			}, {
+				name: 'African Elephant',
+				status: 'Vulnerable'
+			}, {
+				name: 'Bigeye Tuna',
+				status: 'Vulnerable'
+			}, {
+				name: 'Dugong',
+				status: 'Vulnerable'
+			}, {
+				name: 'Forest Elephant',
+				status: 'Vulnerable'
+			}, {
+				name: 'Giant Tortoise',
+				status: 'Vulnerable'
+			}, {
+				name: 'Great White Shark',
+				status: 'Vulnerable'
+			}, {
+				name: 'Greater One-Horned Rhino',
+				status: 'Vulnerable'
+			}, {
+				name: 'Hippopotamus',
+				status: 'Vulnerable'
+			}, {
+				name: 'Irrawaddy Dolphin',
+				status: 'Vulnerable'
+			}, {
+				name: 'Marine Iguana',
+				status: 'Vulnerable'
+			}, {
+				name: 'Olive Ridley Turtle',
+				status: 'Vulnerable'
+			}, {
+				name: 'Polar Bear',
+				status: 'Vulnerable'
+			}, {
+				name: 'Red Panda',
+				status: 'Vulnerable'
+			}, {
+				name: 'Savanna Elephant',
+				status: 'Vulnerable'
+			}, {
+				name: 'Southern Rockhopper Penguin',
+				status: 'Vulnerable'
+			}, {
+				name: 'Whale Shark',
+				status: 'Vulnerable'
+			}, {
+				name: 'Albacore Tuna',
+				status: 'Near Threatened'
+			}, {
+				name: 'Beluga',
+				status: 'Near Threatened'
+			}, {
+				name: 'Greater Sage-Grouse',
+				status: 'Near Threatened'
+			}, {
+				name: 'Jaguar',
+				status: 'Near Threatened'
+			}, {
+				name: 'Monarch Butterfly',
+				status: 'Near Threatened'
+			}, {
+				name: 'Mountain Plover',
+				status: 'Near Threatened'
+			}, {
+				name: 'Narwhal',
+				status: 'Near Threatened'
+			}, {
+				name: 'Plains Bison',
+				status: 'Near Threatened'
+			}, {
+				name: 'White Rhino',
+				status: 'Near Threatened'
+			}, {
+				name: 'Yellowfin Tuna',
+				status: 'Near Threatened'
+			}
+		];
 
+		console.log(animals.length);
+
+	}
+		
+	animalTastic();
 
 });
